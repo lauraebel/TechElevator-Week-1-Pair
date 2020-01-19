de1d91a0f8cc19e899306c2c0c2d4dacbd2f8ed9
Laura Ebel <laura.ebel7@gmail.com>
2020-01-19 16:16:06 -0500

    Pair Exercises Completed

--- src/main/java/com/techelevator/MakeChange.java
+++ src/main/java/com/techelevator/MakeChange.java
@@ -1,4 +1,6 @@
 package com.techelevator;
+import java.util.Scanner;
+
 
 /*
  Write a command line program which prompts the user for the total bill, and the amount tendered. It should then
@@ -9,10 +11,26 @@ package com.techelevator;
  Please enter the amount tendered: 100.00
  The change required is 76.35
  */
+
+// paid - bill = change
+
 public class MakeChange {
 
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
 
+		System.out.print("Enter your total bill amount: ");
+		String userInput = in.nextLine();
+		
+		double totalBill = Double.parseDouble(userInput);
+		
+		System.out.print("Enter how much you paid: ");
+		String userDollars = in.nextLine();
+		
+		double totalPaid = Double.parseDouble(userDollars);
+		
+		double changeRequired = totalPaid - totalBill;
+		System.out.print("Your change is: " + changeRequired);
 	}
 
 }
